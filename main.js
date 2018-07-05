8d0a41f12f51d559b310b877731af9ae3598250d
musi <957028613@qq.com>
2018-07-06 01:48:06 +0800

    兼容移动端

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
 var eraser = document.getElementById('eraser')
-
-canvasSize(canvas)
-
-listenToMouse(canvas)
-
-
-
-
+var isPainting = false
 var isEraser = false
+var lastPoint = {
+    x: undefined, 
+    y: undefined
+}
+
+setCanvasSize(canvas)
+
+if (document.body.ontouchstart !== undefined) {
+    listenToTouch(canvas)
+} else {
+    listenToMouse(canvas)
+}
+
 eraser.onclick = function(e) {
     isEraser = !isEraser
 }
@@ -29,7 +35,7 @@ function drawLine(x, y, x1, y1) {
     context.closePath()
 }
 
-function canvasSize(canvas) {
+function setCanvasSize(canvas) {
     var pageWidth = document.documentElement.clientWidth
     var pageHeight = document.documentElement.clientHeight
     canvas.width = pageWidth
@@ -37,13 +43,6 @@ function canvasSize(canvas) {
 }
 
 function listenToMouse(canvas) {
-    var isPainting = false
-
-    var lastPoint = {
-        x: undefined, 
-        y: undefined
-    }
-
     canvas.onmousedown = function(e) {
         isPainting = true
         var x = e.clientX
@@ -73,6 +72,40 @@ function listenToMouse(canvas) {
     }
 
     canvas.onmouseup = function(e) {
+        isPainting = false
+    }
+}
+
+function listenToTouch(canvas) {
+    canvas.ontouchstart = function(e) {
+        isPainting = true
+        var x = e.touches['0'].clientX
+        var y = e.touches['0'].clientY
+        if (isEraser) {
+            context.clearRect(x-7.5, y-7.5, 15, 15)
+        } else {
+            drawCircle(x, y, 2.2)
+            lastPoint.x = x
+            lastPoint.y = y
+        }
+    }
+    
+    canvas.ontouchmove = function(e) {
+        if (isPainting) {
+            var x = e.touches['0'].clientX
+            var y = e.touches['0'].clientY      
+            if (isEraser) {
+                context.clearRect(x-7.5, y-7.5, 15, 15)
+            } else {
+                drawCircle(x, y, 2.2)
+                drawLine(lastPoint.x, lastPoint.y, x, y)
+                lastPoint.x = x
+                lastPoint.y = y
+            } 
+        }
+    }
+    
+    canvas.ontouchend = function(e) {
         isPainting = false
     }
 }
