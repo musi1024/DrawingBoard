5599b3615f2005c2281a9984034cf941571aee9c
musi <957028613@qq.com>
2018-07-06 22:57:53 +0800

    增加清除下载画笔大小功能

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
-var eraser = document.getElementById('eraser')
 var isPainting = false
 var isEraser = false
+var lineWidth = 5
+var radius = 2.2 
 var lastPoint = {
     x: undefined, 
     y: undefined
@@ -10,17 +11,70 @@ var lastPoint = {
 
 setCanvasSize(canvas)
 
+pen.onclick =function() {
+    isEraser = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+} 
+
+eraser.onclick = function() {
+    isEraser = true
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+}
+
+clear.onclick = function() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+download.onclick = function() {
+    var url = canvas.toDataURL("image/png")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = 'picture'
+    a.click()
+}
+
+colors.onclick = function(e) {
+    var color = e.target.classList.value
+    if (color != 'colors') {
+        removeClassAll('colorActive')
+        e.target.classList.add('colorActive')
+        context.fillStyle = color
+        context.strokeStyle = color
+    }
+}
+
+sizes.onclick = function(e) {
+    var size = e.target.classList.value
+    console.log(size)
+    if (size == 'thin') {
+        lineWidth = 5
+        radius = 2.4
+        removeClassAll
+    } else if(size == 'thick') {
+        lineWidth = 10
+        radius = 4.8
+    } 
+}
+
+function removeClassAll(className) {
+    var selector = '.' + className
+    var elements = document.querySelectorAll(selector)
+    for (var i = 0; i < elements.length; i++) {
+        var e = elements[i]
+        e.classList.remove(className)
+    }
+}
+
 if (document.body.ontouchstart !== undefined) {
     listenToTouch(canvas)
 } else {
     listenToMouse(canvas)
 }
 
-eraser.onclick = function(e) {
-    isEraser = !isEraser
-}
-
-function drawCircle(x, y, radius) {
+function drawCircle(x, y) {
     context.beginPath()
     context.arc(x, y, radius, 0 , 360)
     context.fill()
@@ -29,7 +83,7 @@ function drawCircle(x, y, radius) {
 function drawLine(x, y, x1, y1) {
     context.beginPath()
     context.moveTo(x, y)
-    context.lineWidth = 5
+    context.lineWidth = lineWidth
     context.lineTo(x1, y1)
     context.stroke()
     context.closePath()
@@ -50,7 +104,7 @@ function listenToMouse(canvas) {
         if (isEraser) {
             context.clearRect(x-7.5, y-7.5, 15, 15)
         } else {
-            drawCircle(x, y, 2.2)
+            drawCircle(x, y)
             lastPoint.x = x
             lastPoint.y = y
         }  
@@ -63,7 +117,7 @@ function listenToMouse(canvas) {
             if (isEraser) {
                 context.clearRect(x-7.5, y-7.5, 15, 15)
             } else {
-                drawCircle(x, y, 2.2)
+                drawCircle(x, y)
                 drawLine(lastPoint.x, lastPoint.y, x, y)
                 lastPoint.x = x
                 lastPoint.y = y
@@ -84,7 +138,7 @@ function listenToTouch(canvas) {
         if (isEraser) {
             context.clearRect(x-7.5, y-7.5, 15, 15)
         } else {
-            drawCircle(x, y, 2.2)
+            drawCircle(x, y)
             lastPoint.x = x
             lastPoint.y = y
         }
@@ -97,7 +151,7 @@ function listenToTouch(canvas) {
             if (isEraser) {
                 context.clearRect(x-7.5, y-7.5, 15, 15)
             } else {
-                drawCircle(x, y, 2.2)
+                drawCircle(x, y)
                 drawLine(lastPoint.x, lastPoint.y, x, y)
                 lastPoint.x = x
                 lastPoint.y = y
