f11b000a5cc6d1b7b71d9fe72a790516521ed6a9
youjunjie <jkmusi1024@gmail.com>
2020-08-05 15:18:20 +0800

    feat: toggle toolWrap

--- src/components/ToolWrap.tsx
+++ src/components/ToolWrap.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components/macro';
+import { motion } from 'framer-motion';
 import vw from 'utils/vw';
 import Icon from 'components/Icon';
 
@@ -13,6 +14,7 @@ interface MainProps {
   show: boolean;
 }
 const Main = styled.div<MainProps>`
+  margin-top: ${vw(20)};
   width: ${vw(200)};
   display: ${p => (p.show ? 'flex' : 'none')};
   align-content: flex-start;
@@ -27,12 +29,33 @@ interface ToolProps {
   children?: React.ReactNode;
 }
 
+const control = {
+  visible: {
+    x: 0
+  },
+  hidden: {
+    x: '-120%'
+  }
+};
+
 const ToolWrap: React.FC<ToolProps> = ({ children }) => {
+  const [onOff, setOnOff] = useState<boolean>(false);
   const [show, setShow] = useState<boolean>(false);
+  const toggle = () => setOnOff(s => !s);
+
   return (
     <Wrap>
-      <Icon type="huabi" onClick={() => setShow(s => !s)} />
-      <Main show={show}>{children}</Main>
+      <Icon type="huabi" onClick={toggle} />
+      <motion.div
+        initial="hidden"
+        animate={onOff ? 'visible' : 'hidden'}
+        variants={control}
+        transition={{ type: 'spring', damping: 300 }}
+        onAnimationStart={() => onOff && setShow(true)}
+        onAnimationComplete={() => !onOff && setShow(false)}
+      >
+        <Main show={show}>{children}</Main>
+      </motion.div>
     </Wrap>
   );
 };
